712999910c35c6987a26fc27acc7255830d346b0
Ivan Simeonov <ivannicksimeonov@gmail.com>
2024-12-07 23:00:00 +0200

    Dashboard optimized

--- src/app/pages/admin-dashboard/admin-dashboard.component.ts
+++ src/app/pages/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,10 @@
-import { Component, inject, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild, AfterViewInit, signal, computed } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
 import {
   AdminControllerService,
@@ -13,18 +13,28 @@ import {
   TopicAnalyticsDto,
   TopicControllerService,
   UserAnalyticsDto,
+  UserTeacherAccessRequestDto,
 } from '@ivannicksim/vlp-backend-openapi-client';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { delay } from 'rxjs';
 
-interface TeacherAccessRequest {
-  firstName: string;
-  lastName: string;
-  email: string;
+interface IChartData {
+  name?: string;
+  value?: number;
 }
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [NgxChartsModule, MatButtonModule, MatTableModule, MatSortModule, MatPaginatorModule, MatIconModule],
+  imports: [
+    NgxChartsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss',
 })
@@ -35,78 +45,125 @@ export class AdminDashboardComponent implements OnInit, AfterViewInit {
   private topicService = inject(TopicControllerService);
 
   displayedColumns: string[] = ['firstName', 'lastName', 'email', 'actions'];
-  dataSource!: MatTableDataSource<TeacherAccessRequest>;
+  dataSource = signal<MatTableDataSource<UserTeacherAccessRequestDto>>(new MatTableDataSource());
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  teacherAccessRequests: TeacherAccessRequest[] = [
-    { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com' },
-    { firstName: 'Jane', lastName: 'Smith', email: 'jane.smith@example.com' },
-    { firstName: 'Alice', lastName: 'Johnson', email: 'alice.johnson@example.com' },
-    { firstName: 'Alice', lastName: 'Johnson', email: 'alice.johnson@example.com' },
-    { firstName: 'Alice', lastName: 'Johnson', email: 'alice.johnson@example.com' },
-    { firstName: 'Alice', lastName: 'Johnson', email: 'alice.johnson@example.com' },
-    { firstName: 'Alice', lastName: 'Johnson', email: 'alice.johnson@example.com' },
-    { firstName: 'Alice', lastName: 'Johnson', email: 'alice.johnson@example.com' },
-    { firstName: 'Alice', lastName: 'Johnson', email: 'alice.johnson@example.com' },
-    { firstName: 'Alice', lastName: 'Johnson', email: 'alice.johnson@example.com' },
-  ];
-
-  userAnalyticsData: { name?: string; value?: number }[] = [];
-  courseAnalyticsData: { name?: string; value?: number }[] = [];
-  topicAnalyticsData: { name?: string; value?: number }[] = [];
+  isLoading = signal(false);
+  userAnalyticsData = signal<IChartData[]>([]);
+  courseAnalyticsData = signal<IChartData[]>([]);
+  topicAnalyticsData = signal<IChartData[]>([]);
+  paginationSorting = signal({ pageNumber: 0, pageSize: 5, sortBy: 'firstName', sortDirection: 'asc' });
+  isTableEmpty = computed(() => this.dataSource()?.data.length === 0);
 
   ngOnInit(): void {
-    this.dataSource = new MatTableDataSource(this.teacherAccessRequests);
+    this.fetchUserTeacherAccessRequests();
     this.fetchCourseAnalyticsData();
     this.fetchUserAnalyticsData();
     this.fetchTopicAnalyticsData();
   }
 
   ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.dataSource().paginator = this.paginator;
+    this.dataSource().sort = this.sort;
   }
 
-  approveRequest(email: string): void {
-    // TODO: Add API call
-    console.log('Approve Request User Email:', email);
+  onPageChange(event: PageEvent): void {
+    this.paginationSorting.update((state) => ({
+      ...state,
+      pageNumber: event.pageIndex,
+      pageSize: event.pageSize,
+    }));
+    this.fetchUserTeacherAccessRequests();
   }
 
-  denyRequest(email: string): void {
-    // TODO: Add API call
-    console.log('Deny Request User Email:', email);
+  onSortChange(event: Sort): void {
+    this.paginator.pageIndex = 0;
+    this.paginationSorting.update((state) => ({
+      ...state,
+      pageNumber: 0,
+      sortBy: event.active,
+      sortDirection: event.direction,
+    }));
+    this.fetchUserTeacherAccessRequests();
+  }
+
+  approveRequest(requestDto: UserTeacherAccessRequestDto): void {
+    this.isLoading.set(true);
+    this.adminService
+      .approveTeacherAccessRequest(requestDto)
+      .pipe(delay(500))
+      .subscribe({
+        next: () => this.handleRequestSuccess(),
+        error: (err) => this.handleRequestError(err),
+      });
+  }
+
+  denyRequest(requestDto: UserTeacherAccessRequestDto): void {
+    this.isLoading.set(true);
+    this.adminService
+      .denyTeacherAccessRequest(requestDto)
+      .pipe(delay(500))
+      .subscribe({
+        next: () => this.handleRequestSuccess(),
+        error: (err) => this.handleRequestError(err),
+      });
   }
 
   navigateTo(route: string): void {
     this.router.navigate([route]);
   }
 
+  private fetchUserTeacherAccessRequests(): void {
+    const { pageNumber, pageSize, sortBy, sortDirection } = this.paginationSorting();
+    this.adminService.getPendingTeacherAccessRequests(pageNumber, pageSize, sortBy, sortDirection).subscribe((data) => {
+      this.dataSource.set(new MatTableDataSource(data.content || []));
+      this.paginator.length = data.totalElements || 0;
+    });
+  }
+
   private fetchCourseAnalyticsData() {
     this.courseService.getCourseAnalytics().subscribe((result: CourseAnalyticsDto[]) => {
-      this.courseAnalyticsData = result.map((res) => ({
-        name: res.difficultyLevel,
-        value: res.totalCourses,
-      }));
+      this.courseAnalyticsData.set(
+        result.map((res) => ({
+          name: res.difficultyLevel,
+          value: res.totalCourses,
+        }))
+      );
     });
   }
 
   private fetchUserAnalyticsData() {
     this.adminService.getUserAnalytics().subscribe((result: UserAnalyticsDto[]) => {
-      this.userAnalyticsData = result.map((res) => ({
-        name: res.userGroup,
-        value: res.totalUsers,
-      }));
+      this.userAnalyticsData.set(
+        result.map((res) => ({
+          name: res.userGroup,
+          value: res.totalUsers,
+        }))
+      );
     });
   }
 
   private fetchTopicAnalyticsData() {
     this.topicService.getTopicAnalytics().subscribe((result: TopicAnalyticsDto[]) => {
-      this.topicAnalyticsData = result.map((res) => ({
-        name: res.title,
-        value: res.totalCourses,
-      }));
+      this.topicAnalyticsData.set(
+        result.map((res) => ({
+          name: res.title,
+          value: res.totalCourses,
+        }))
+      );
     });
+  }
+
+  private handleRequestSuccess() {
+    this.isLoading.set(false);
+    this.fetchUserTeacherAccessRequests();
+    this.fetchUserAnalyticsData();
+  }
+
+  private handleRequestError(err: Error) {
+    this.isLoading.set(false);
+    console.error('Error: ', err);
   }
 }
