623eec353ce86556ea5c8dcdbaa8989824708ef5
Ivan Simeonov <ivannicksimeonov@gmail.com>
2024-12-14 22:13:48 +0200

    Interceptor fixed to logout user on expired token

--- src/app/auth/interceptors/auth.interceptor.ts
+++ src/app/auth/interceptors/auth.interceptor.ts
@@ -1,22 +1,31 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   private authService = inject(AuthService);
+  private router = inject(Router);
   private readonly WHITE_LISTED_URLS = ['/api/v1/courses'];
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const authToken = this.authService.getAccessToken();
     const isWhiteListed = this.WHITE_LISTED_URLS.some((url) => request.url.includes(url));
     if (authToken && !isWhiteListed) {
-      const authReq = request.clone({
+      request = request.clone({
         headers: request.headers.set('Authorization', `Bearer ${authToken}`),
       });
-      return next.handle(authReq);
     }
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401 || error.status === 403) {
+          this.authService.clearTokens();
+          this.router.navigate(['/login']);
+        }
+        return throwError(() => error);
+      })
+    );
   }
 }
