9f1fe62dcbde9404baced926d9f632bc655954f0
Ivan Simeonov <ivannicksimeonov@gmail.com>
2024-12-14 22:38:29 +0200

    Login Form validation added

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal, WritableSignal } from '@angular/core';
 import { FormsModule, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,6 +6,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../auth/services/auth.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -29,12 +31,19 @@ export class LoginComponent {
   private router = inject(Router);
   private route = inject(ActivatedRoute);
   hide = signal(true);
+  emailErrorMsg = signal('');
+  passwordErrorMsg = signal('');
   errorResponse = signal<string | null>(null);
+  formSubmitted = false;
 
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]],
   });
+
+  constructor() {
+    this.initErrorSubscriptions();
+  }
 
   togglePasswordVisibility(event: MouseEvent) {
     this.hide.set(!this.hide());
@@ -42,6 +51,7 @@ export class LoginComponent {
   }
 
   login() {
+    this.formSubmitted = true;
     if (this.loginForm.valid) {
       this.authService
         .login({
@@ -58,6 +68,53 @@ export class LoginComponent {
             console.error('Error: ', err);
           },
         });
+    } else {
+      Object.values(this.loginForm.controls).forEach((control) => {
+        control.markAsTouched();
+        control.updateValueAndValidity();
+      });
     }
+  }
+
+  private initErrorSubscriptions(): void {
+    const fields: { name: string; signal: WritableSignal<string>; messages: Record<string, string> }[] = [
+      {
+        name: 'email',
+        signal: this.emailErrorMsg,
+        messages: {
+          required: 'Email is required',
+          email: 'Please enter a valid email address',
+        },
+      },
+      {
+        name: 'password',
+        signal: this.passwordErrorMsg,
+        messages: {
+          required: 'Password is required',
+          minlength: 'Password must be at least 8 characters long',
+          maxlength: 'Password cannot exceed 16 characters',
+        },
+      },
+    ];
+    fields.forEach(({ name, signal, messages }) => {
+      merge(this.loginForm.get(name)!.statusChanges, this.loginForm.get(name)!.valueChanges)
+        .pipe(takeUntilDestroyed())
+        .subscribe(() => this.updateErrorMsg(name, signal, messages));
+    });
+  }
+
+  private updateErrorMsg(
+    controlName: string,
+    errorMsgSignal: WritableSignal<string>,
+    messages: Record<string, string>
+  ): void {
+    const control = this.loginForm.get(controlName);
+    for (const [errorKey, errorMsg] of Object.entries(messages)) {
+      if (control?.hasError(errorKey)) {
+        errorMsgSignal.set(errorMsg);
+        return;
+      }
+    }
+    errorMsgSignal.set('');
   }
 }
